4188891acc023231ae45283ffaf16c4e6eaf55ba
Saddam Abu Ghaida <saddam.abu@deliveryhero.com>
2026-01-27 10:45:34 +0100

    add patch,update rbac for events

--- internal/controller/rbac.go
+++ internal/controller/rbac.go
@@ -7,6 +7,6 @@
 // +kubebuilder:rbac:groups=networking.istio.io,resources=virtualservices,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;delete
 // +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
-// +kubebuilder:rbac:groups="",resources=events,verbs=create
+// +kubebuilder:rbac:groups="",resources=events,verbs=watch;create;patch;update
 // +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch
 package controller
